483fd4f24f27fa4a60e869a5efadf9b64b70477f
juan-gasero <159394842+juan-gasero@users.noreply.github.com>
2025-05-13 18:23:46 -0700

    Gastelum Quintero Juan Fernando 13/05/25

---
+++ CONNECT.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"; // usuario
+$password = ""; // Por defecto, la contraseña está vacía
+$dbname = "recetario";
+// Crear conexión a server
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Verificar conexión a server
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}else{
+    echo "<h1>Conexion exitosa</h1>";
+}
+?>
